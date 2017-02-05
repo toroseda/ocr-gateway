17f2a5566f0c227a5a72fb9bac07d6f17bf1e936
Olutoye Akande <olutoyeakande@Olutoyes-MacBook-Pro.local>
2017-02-05 18:19:06 +0400

    initial commit

---
+++ src/main/webapp/app/blocks/config/localstorage.config.js
@@ -0,0 +1,14 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('ocrGatewayApp')
+        .config(localStorageConfig);
+
+    localStorageConfig.$inject = ['$localStorageProvider', '$sessionStorageProvider'];
+
+    function localStorageConfig($localStorageProvider, $sessionStorageProvider) {
+        $localStorageProvider.setKeyPrefix('jhi-');
+        $sessionStorageProvider.setKeyPrefix('jhi-');
+    }
+})();
---
+++ src/main/webapp/app/components/form/pagination.constants.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('ocrGatewayApp')
+        .constant('paginationConstants', {
+            'itemsPerPage': 20
+        });
+})();
